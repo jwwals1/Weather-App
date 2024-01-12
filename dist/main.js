89621725699be05c3d900ce0b0cad683018a9f53
jwwals1 <joe_walsh59@yahoo.com>
2024-01-11 20:22:25 -0600

    updated main.js

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("test");
+(()=>{"use strict";const e=document.querySelector("div");document.getElementById("searchButton").addEventListener("click",(()=>{!async function(){const t=document.getElementById("searchbar").value,n=await fetch(`https://api.weatherapi.com/v1/current.json?key=6425748fc6174f0882a173729241001&q=${t}`,{mode:"cors"}),c=await n.json();e.innerHTML=`the current temperature in ${t} \n        is ${c.current.temp_f}° and it feels like \n        ${c.current.feelslike_f}° Condition: ${c.current.condition.text}`,console.log(c)}()}))})();
